b04355275d49667f2a8261ea11a9393b36978e18
montero-melis <montero.gui@gmail.com>
2018-12-21 19:04:03 +0100

    Starts script for the simulation of semantic priming task

---
+++ checks/sem-priming_simulations.R
@@ -0,0 +1,18 @@
+## Simulate data from semantic priming task to get a feeling for power and
+## other characteristics of the data and analyses.
+
+
+# TO DO:
+
+# Basic procedure: For each set of simulated data, check:
+#   
+#   1) Significant priming effect?
+#   2) Is the correlation between the actual by-subject random adjustment and the BLUP significant?
+# 
+# Manipulate:
+# - For starters, simulate data with values based on SPP
+# - Then reduce the priming effect and/or augment the resid. noise
+# - Change the number of items and subjects
+# - Change the variability between subjects and items
+
+
